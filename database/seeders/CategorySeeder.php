be60a1adc834a1551a41fe22b21ab718a9705cc7
moofx19 <mahmoudofx@gmail.com>
2025-06-01 12:55:07 +0300

    feat: Implement CategorySeeder to populate categories with localized names and random data

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,9 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use Faker\Factory as Faker;
 class CategorySeeder extends Seeder
 {
     /**
@@ -12,6 +13,18 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $faker = Faker::create();
+        $categories = ['Painting', 'Drawings', 'Mixed Media', 'Sculpture', 'Collage', 'Digital', 'Printmaking', 'Installation'];
+        $categories_ar = ['لوحة', 'رسومات', 'وسائط مختلطة', 'منحوتة', 'كولاج', 'رقمي', 'طباعة', 'تركيب'];
+        foreach($categories as $index => $category){
+            Category::create([
+                'name'=> $category,
+                'name_ar'=> $categories_ar[$index],
+                'cover_image'=> $faker->imageUrl(640, 480, 'art', true),
+                'description'=> $faker->sentence,
+                'description_ar'=> $faker->sentence,
+                'link'=> $faker->url
+            ]);
+        }
     }
 }
