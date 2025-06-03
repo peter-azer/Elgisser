bd08548f38e493f83fbc269a671cc2cad9e05dac
moofx19 <mahmoudofx@gmail.com>
2025-06-03 12:46:13 +0300

    fix: Refactor rental code generation to use consistent format and simplify logic

--- app/Services/RentedArtworkNumberService.php
+++ app/Services/RentedArtworkNumberService.php
@@ -10,11 +10,7 @@ class RentedArtworkNumberService
     public static function generate()
     {
         $latest = RentedArtWork::latest('id')->first();
-        if ($latest && preg_match('/RENT-(\d+)/', $latest->number, $matches)) {
-            $number = intval($matches[1]) + 1;
-        } else {
-            $number = 1;
-        }
-        return 'RENT-' . str_pad($number, 5, '0', STR_PAD_LEFT);
+        $number = $latest ? intval(substr($latest->rental_code, 5)) + 1 : 1;
+        return 'Rent-' . str_pad($number, 5, '0', STR_PAD_LEFT);
     }
 }
