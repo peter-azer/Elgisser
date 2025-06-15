90444f5180a3a9ac97fa1b17534c17e627072fc8
abood <>
2025-06-15 15:24:23 +0300

    Add CategorySeeder and Filter to DatabaseSeeder for initial data population

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             RolePermissionSeeder::class,
+            CategorySeeder::class,
+            Filter::class,
         ]);
     }
 }
