5b1f192a0ca93ffd7c2c52db84c121d4b1e1293b
moofx19 <mahmoudofx@gmail.com>
2025-06-29 17:15:59 +0300

    Fix order number generation to use correct property from Order model

--- app/Services/OrderNumberService.php
+++ app/Services/OrderNumberService.php
@@ -10,7 +10,7 @@ class OrderNumberService
     public static function generate()
     {
         $latest = Order::latest('id')->first();
-        $number = $latest ? intval(substr($latest->order_id, 5)) + 1 : 1;
+        $number = $latest ? intval(substr($latest->order_number, 5)) + 1 : 1;
         return 'ORDR-' . str_pad($number, 5, '0', STR_PAD_LEFT);
     }
 }
