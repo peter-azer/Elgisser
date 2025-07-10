8e71cb9d204d3d56cefd9491deade9405fcbcdec
moofx19 <mahmoudofx@gmail.com>
2025-07-10 11:03:00 +0300

    Restore 'http://localhost:5173' to allowed_origins in CORS configuration and remove SoftDeletes trait from Category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,11 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
-    use SoftDeletes;
     /** @use HasFactory<\Database\Factories\CategoryFactory> */
     use HasFactory;
 
