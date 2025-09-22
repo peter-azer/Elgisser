f6f41afb2ccbede46fa59eaf63b5aebf16bdb724
moofx19 <mahmoudofx@gmail.com>
2025-09-22 17:48:53 +0300

    test

--- app/Http/Controllers/Payment/PaymentController.php
+++ app/Http/Controllers/Payment/PaymentController.php
@@ -32,7 +32,7 @@ class PaymentController extends Controller
         $response = $this->paymentGateway->callBack($request);
 
         if ($response) {
-            return redirect()->away('http://localhost:5173/cart?status=success');
+            return redirect()->away('http://localhost:5173/cart?status=success')->with(['request' => $request->all()]);
         }
         return redirect()->away('http://localhost:5173/payment/failure?status=failed');
     }
